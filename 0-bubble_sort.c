ad7716011f06fa716502bd1533b94f0636b87d90
lacroixpaul <paul.lacroix892@outlook.fr>
2024-12-11 11:39:31 +0100

    bubble_sort bool rm and size < 3 added

--- 0-bubble_sort.c
+++ 0-bubble_sort.c
@@ -1,5 +1,4 @@
 #include "sort.h"
-#include <stdbool.h>
 
 /**
 * bubble_sort - sorts an array of integers using the Bubble sort algorithm.
@@ -11,11 +10,13 @@
 void bubble_sort(int *array, size_t size)
 {
 	unsigned int i = 0, j = 0, temp = 0;
-	bool swapped;
+	int swapped;
 
+	if (size < 3)
+		return;
 	for (i = 0; i < size - 1; i++)
 	{
-		swapped = false;
+		swapped = 0;
 		for (j = 0; j < size - i - 1; j++)
 		{
 			if (array[j] > array[j + 1])
@@ -23,11 +24,11 @@ void bubble_sort(int *array, size_t size)
 				temp = array[j];
 				array[j] = array[j + 1];
 				array[j + 1] = temp;
-				swapped = true;
+				swapped = 1;
 			}
 			print_array(array, size);
 		}
-		if (swapped == false)
+		if (swapped == 0)
 			break;
 	}
 }
