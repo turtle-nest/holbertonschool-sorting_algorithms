9ad5aeb22c5725a7dda4a766fb9d15642f106a91
lacroixpaul <paul.lacroix892@outlook.fr>
2024-12-11 11:56:59 +0100

    bubble_sort  print_array moved

--- 0-bubble_sort.c
+++ 0-bubble_sort.c
@@ -26,8 +26,8 @@ void bubble_sort(int *array, size_t size)
 				array[j + 1] = temp;
 				swapped = 1;
 			}
-			print_array(array, size);
 		}
+		print_array(array, size);
 		if (swapped == 0)
 			break;
 	}
